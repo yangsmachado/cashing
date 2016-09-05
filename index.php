3c6302cfce915be91262172f6cde73df4da9d212
yangsmachado <yansilvagabriel@gmail.com>
2016-09-04 21:17:23 -0300

    Prova Remota TeckLabs - Leia o arquivo readme.pdf

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-    ini_set('display_errors', '1');
-    
+    // Os codigos e comentarios podem ficar muitos longos em uma mesma linha pois a ide que utilizei possui o recurso de Word Wrap que as quebra para mim.
+
     // Checa se o sistema ja esta instalado antes de tentar a instalacao
     if (!file_exists('config/System.php')) {
         header('Location: setup');
